8a4c92ddc5a584cc28fdccde398210eeb3bcd0a4
Vladislav Obukhov <vdaobukhov8@gmail.com>
2026-09-25 01:27:46 +0300

    Implement spider movement

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,28 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.addEventListener('click', (e) => {
+  const wallRect = wall.getBoundingClientRect();
+  const spiderRect = spider.getBoundingClientRect();
+
+  const spiderWidth = spiderRect.width;
+  const spiderHeight = spiderRect.height;
+
+  let leftPosition = e.clientX - wallRect.left - spiderWidth / 2;
+  let topPosition = e.clientY - wallRect.top - spiderHeight / 2;
+
+  leftPosition = Math.max(
+    0,
+    Math.min(leftPosition, wallRect.width - spiderWidth),
+  );
+
+  topPosition = Math.max(
+    0,
+    Math.min(topPosition, wallRect.height - spiderHeight),
+  );
+
+  spider.style.left = `${leftPosition}px`;
+  spider.style.top = `${topPosition}px`;
 });
