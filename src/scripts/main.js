40405938a053b3dde95374efa4c10981bf4adb7d
Vladislav Obukhov <vdaobukhov8@gmail.com>
2026-09-25 01:36:55 +0300

    Fix spider position calculation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,22 +5,24 @@ const spider = document.querySelector('.spider');
 
 wall.addEventListener('click', (e) => {
   const wallRect = wall.getBoundingClientRect();
-  const spiderRect = spider.getBoundingClientRect();
 
-  const spiderWidth = spiderRect.width;
-  const spiderHeight = spiderRect.height;
+  const spiderWidth = spider.offsetWidth;
+  const spiderHeight = spider.offsetHeight;
 
-  let leftPosition = e.clientX - wallRect.left - spiderWidth / 2;
-  let topPosition = e.clientY - wallRect.top - spiderHeight / 2;
+  let leftPosition =
+    e.clientX - wallRect.left - wall.clientLeft - spiderWidth / 2;
+
+  let topPosition =
+    e.clientY - wallRect.top - wall.clientTop - spiderHeight / 2;
 
   leftPosition = Math.max(
     0,
-    Math.min(leftPosition, wallRect.width - spiderWidth),
+    Math.min(leftPosition, wall.clientWidth - spiderWidth),
   );
 
   topPosition = Math.max(
     0,
-    Math.min(topPosition, wallRect.height - spiderHeight),
+    Math.min(topPosition, wall.clientHeight - spiderHeight),
   );
 
   spider.style.left = `${leftPosition}px`;
